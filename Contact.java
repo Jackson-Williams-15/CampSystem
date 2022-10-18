82941cf3c6bc61e8462293995f2f7120fe225261
Jackson Williams <wjack1201@yahoo.com>
2022-10-18 13:57:42 -0400

    added contact class stubs

---
+++ Contact.java
@@ -0,0 +1,11 @@
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String email;
+
+    public Contact(String firstName, String lastName, int phoneNumber, String email){
+        
+    }
+}
