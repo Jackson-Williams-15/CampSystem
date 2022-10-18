2b1f4c20a59b5932a96685c00822c103c3407445
Jackson Williams <wjack1201@yahoo.com>
2022-10-18 13:55:18 -0400

    added user class stubs

---
+++ User.java
@@ -0,0 +1,17 @@
+
+public class User {
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected Date dateOfBirth;
+    protected int phone;
+    protected String password;
+    
+    public User(String Name, int number, String email, String password){
+
+    }
+
+    protected Contact getContact(){
+        return null;
+    }
+}
