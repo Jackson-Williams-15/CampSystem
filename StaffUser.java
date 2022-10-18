170682ec331645e57b1a766b1ac7437a21b70f29
Jackson Williams <wjack1201@yahoo.com>
2022-10-18 13:55:02 -0400

    adding StaffUser stubs

---
+++ StaffUser.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class StaffUser {
+    private ArrayList<Contact> emergencyContact;
+    private Contact doctor;
+    public ArrayList<String> allergies;
+    private ArrayList<Medication> medication;
+    private Date birthday;
+
+    public ArrayList<String> getAllAllergies(){
+
+    }
+
+    public boolean checkAllergies(String allergies){
+
+    } 
+
+    public void staffSchedule(){
+
+    }
+
+    public String toString(){
+        
+    }
+}
