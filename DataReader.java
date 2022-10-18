c662ea698a474e4ff2d537dde572996ed8008726
Jackson Williams <wjack1201@yahoo.com>
2022-10-18 14:26:56 -0400

    added DataReader stubs

---
+++ DataReader.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class DataReader {
+    public ArrayList<User> getAllUsers(){
+        return null;
+    }
+
+    public Camp getCamp(){
+        return null;
+    }
+    
+}
