e30b94591881c9d8c4c158a2d220eb8787dc1635
christinee-xu <christinexu1228@gmail.com>
2022-10-18 14:10:21 -0400

    adding medication class

---
+++ Medication.java
@@ -0,0 +1,11 @@
+public class Medication {
+    private String type;
+    private String dose;
+    private String time;
+
+    public Medication(String type, String dose, String time) {
+        this.type = type;
+        this.dose = dose;
+        this.time = time;
+    }
+}
