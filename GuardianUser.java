b5aa2113da058c89d397b8fb4b92028a1ef3f490
Jackson Williams <wjack1201@yahoo.com>
2022-10-18 14:17:43 -0400

    updated GuardianUser Stubs

--- GuardianUser.java
+++ GuardianUser.java
@@ -1,8 +1,19 @@
 import java.util.ArrayList;
 
 public class GuardianUser{
-    private ArrayList<User> userProfile;
-    public ArrayList<User> seeProfiles(){
-        return this.userProfile;
+
+    private ArrayList<Child> children;
+    private int campDuration;
+    private Date campDates;
+    public void addChild(Child child){
+        this.children.add(child);
+    }
+    public Child getChild(String name){
+        Child theChild;
+        for (Child child : children) {
+            if(child.firstName = name)
+                theChild = child;
+        }
+        return theChild;
     }
 }
