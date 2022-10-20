062cb2e62b45ca6b12c7d51e9555f45b9617dc8b
CWASH2002 <csw11@email.sc.edu>
2022-10-20 14:03:03 -0400

    Schedule

--- Child.java
+++ Child.java
@@ -14,48 +14,11 @@ public class Child{
         this.birthdate = birthdate;
 
     }
-<<<<<<< HEAD
-=======
-
-    // Copy Constructor
-    public Child(Child newChild){
-        this.firstName = newChild.getFirstName();
-        this.lastName = newChild.getLastName();
-        this.emergencyContacts = newChild.getContact();
-        this.birthdate = newChild.getBirth();
-        this.allergies = newChild.getAllergies();
-        this.medications = newChild.getMeds();
-    }
-    // Setters
-
->>>>>>> ea775a0ecf9be83690cef6716938b29c015d0f8c
     public void addMedication(Medication med){
         this.medications.add(med);
     }
     public void addContact(Contact contact){
         this.emergencyContacts.add(contact);
     }
-<<<<<<< HEAD
-=======
-    // Getter
-    public String getFirstName(){
-        return this.firstName;
-    }
-    public String getLastName(){
-        return this.lastName;
-    }
-    public Date getBirth(){
-        return this.birthdate;
-    }
-    public ArrayList<String> getAllergies(){
-        return this.allergies;
-    }
-    public ArrayList<Contact> getContact(){
-        return this.emergencyContacts;
-    }
-    public ArrayList<Medication> getMeds(){
-        return this.medications;
-    }
->>>>>>> ea775a0ecf9be83690cef6716938b29c015d0f8c
     
 }
