6b2bb639e750b8b390567671f94b8223517aef88
christinee-xu <christinexu1228@gmail.com>
2022-10-20 13:57:47 -0400

    updating data constants

--- DataConstants.java
+++ DataConstants.java
@@ -1,3 +1,9 @@
 public class DataConstants {
-    
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_EMAIL = "email";
+    protected static final String USER_DOB = "dateOfBirth";
+    protected static final String USER_PHONE_NUMBER = "phoneNumber";
+    protected static final String USER_PASSWORD = "password";
+    protected static final String USER_CONTACT = "contact";
 }
