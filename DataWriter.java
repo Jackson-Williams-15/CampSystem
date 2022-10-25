ee5a08d1e81b9927f44ab9cbb80d2fbe09ef5ea7
Jackson Williams <wjack1201@yahoo.com>
2022-10-25 14:07:03 -0400

    started DataWriter

--- DataWriter.java
+++ DataWriter.java
@@ -1,8 +1,20 @@
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class DataWriter extends DataConstants {
-    public boolean saveAllUsers(){
-        return false;
+    public void saveAllUsers(){
+        UserList user = UserList.getInstance();
+		ArrayList<UserList> users = user.getUser();
+		JSONArray jsonUser = new JSONArray();
+		
+		//creating all the json objects
+		for(int i=0; i< user.size(); i++) {
+			jsonUser.add(getPersonJSON(user.get(i)));
+		}
 
     }
     
