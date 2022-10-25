50638c39c4224d60fdb430b6f4f95d76575c315b
christinee-xu <christinexu1228@gmail.com>
2022-10-25 13:24:08 -0400

    adding user id and file name

--- DataConstants.java
+++ DataConstants.java
@@ -1,4 +1,6 @@
 public class DataConstants {
+    protected static final String USER_FILE_NAME = "json/users.json";
+    protected static final String USER_ID = "id";
     protected static final String USER_FIRST_NAME = "firstName";
     protected static final String USER_LAST_NAME = "lastName";
     protected static final String USER_EMAIL = "email";
