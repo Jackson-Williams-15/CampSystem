d4753e80b45e533e2d0fc63059241eb402ae9821
Jackson Williams <wjack1201@yahoo.com>
2022-10-27 13:41:49 -0400

    updated data constants

--- DataConstants.java
+++ DataConstants.java
@@ -8,4 +8,11 @@ public class DataConstants {
     protected static final String USER_PHONE_NUMBER = "phone";
     protected static final String USER_PASSWORD = "password";
     protected static final String USER_CONTACT = "contact";
+    protected static final String CABIN_FILE_NAME = "json/cabins.json";
+    protected static final String CABIN_NAME = "CABIN";
+    protected static final String CHILD_FILE_NAME = "json/cabins.json";
+    protected static final String CHILD_FIRST_NAME = "firstName";
+    protected static final String CHILD_LAST_NAME = "lastName";
+    protected static final String CHILD_DOB = "dateOfBirth";
+    protected static final String CHILD_ALLERGIES = "allergies";
 }
