822b77524e5cb46bf950b4f65f03e997fbb17a45
Jackson Williams <wjack1201@yahoo.com>
2022-10-31 22:01:24 -0400

    Updated camp for data writer

--- Camp.java
+++ Camp.java
@@ -1,13 +1,30 @@
 public class Camp 
 {
     private String name;
+    private String theme;
+    private String cabin;
     private UserList userList;
     private int availability;
     private int sessions;
 
-    public Camp(String name){
-        this.name = name;
+    public Camp(Camp newCamp){
+        this.name = newCamp.getName();
+        this.theme = newCamp.getTheme();
+        this.cabin = newCamp.getCabins();
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getTheme(){
+        return this.theme;
+    }
+
+    public String getCabins(){
+        return this.cabin;
+    }
+    
     public Child findChild(String name, int age){
         return null;
     }
