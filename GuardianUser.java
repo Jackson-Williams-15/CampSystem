703bd7118f877625aaf478fe9e36baea65c8954e
Caleb Martin <cam228@outlook.com>
2022-11-02 17:39:18 -0400

    Changed campDates to ArrayList

--- GuardianUser.java
+++ GuardianUser.java
@@ -5,9 +5,9 @@ public class GuardianUser extends User {
 
     private ArrayList<Child> children;
     private int campDuration;
-    private String campDates;
+    private ArrayList<String> campDates;
 
-    public GuardianUser(String firstName, String lastName, int phoneNumber, String email, String password, String dateOfBirth, UUID id, ArrayList<Child> children, int campDuration, String campDates) {
+    public GuardianUser(String firstName, String lastName, int phoneNumber, String email, String password, String dateOfBirth, UUID id, ArrayList<Child> children, int campDuration, ArrayList<String> campDates) {
         super(firstName, lastName, phoneNumber, email, password, dateOfBirth, id);
         this.children = children;
         this.campDuration = campDuration;
