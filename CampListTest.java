6ee0cd34b52126021b06d8139d4c202c29ac48c3
Caleb Martin <cam228@outlook.com>
2022-11-09 17:08:30 -0500

    Caleb has called dibs on these test files

---
+++ CampListTest.java
@@ -0,0 +1,13 @@
+/**
+ * @author Caleb Martin
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.*;
+
+public class CampListTest
+{
+    
+}
---
+++ ChildListTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author Caleb Martin
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.*;
+
+public class ChildListTest {
+    
+}
