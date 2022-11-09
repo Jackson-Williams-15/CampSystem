debf823461163df0b71508bc3b636f34c0bde8a2
Caleb Martin <cam228@outlook.com>
2022-11-09 17:02:38 -0500

    Added JUnit and first test

---
+++ CabinListTest.java
@@ -0,0 +1,48 @@
+/**
+ * @author Caleb Martin
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.*;
+
+public class CabinListTest
+{
+    private static ChildList childList;
+    private static UserList userList;
+    private static CabinList cabinList;
+
+    @BeforeClass
+    public static void oneTimeSetup()
+    {
+        childList = ChildList.getInstance();
+        userList = UserList.getInstance();
+        cabinList = CabinList.getInstance();
+    }
+
+    @AfterClass
+    public static void oneTimeTeardown()
+    {
+        
+    }
+    
+    @Test
+    public void testInstantiation()
+    {
+        assertNotNull(cabinList);
+    }
+
+    @Test
+    public void testDataInstantiation()
+    {
+        assertNotNull(cabinList.getCabins());
+    }
+
+    @Test
+    public void testGetCabin()
+    {
+        Cabin gottenCabin = cabinList.getCabin(UUID.fromString("dd30acc7-6e51-4174-9fd0-4f345915fbe8"));
+        assertEquals(gottenCabin.getName(), "Brown Bear");
+    }
+}
