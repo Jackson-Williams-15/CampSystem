632231212a0cdd727fa0a4c78e620823bea4e1d7
Caleb Martin <cam228@outlook.com>
2022-11-10 15:57:25 -0500

    Added tests for constructors that should'nt work

--- ActivityTest.java
+++ ActivityTest.java
@@ -11,11 +11,17 @@ import java.util.ArrayList;
 public class ActivityTest
 {
     private Activity testActivity;
+    private Activity nullActivity;
+    private Activity blankActivity;
+    private Activity badActivity;
     
     @Before
     public void everyTimeSetup()
     {
         testActivity = new Activity(Type.CREATIVE, "Testing", "1:00 PM");
+        nullActivity = new Activity(null, null, null);
+        blankActivity = new Activity(Type.CREATIVE, "", "");
+        badActivity = new Activity(Type.CREATIVE, "Testing", "InvalidTimeString");
     }
 
     @Test
@@ -39,5 +45,47 @@ public class ActivityTest
     public void testToString()
     {
         assertEquals("Testing\n1:00 PM\nnull\n", testActivity.toString());
+    }
+
+    @Test
+    public void testConstructNullType()
+    {
+        assertTrue(nullActivity == null || nullActivity.getType() != null);
+    }
+
+    @Test
+    public void testConstructNullName()
+    {
+        assertTrue(nullActivity == null || nullActivity.getName() != null);
+    }
+
+    @Test
+    public void testConstructBlankName()
+    {
+        assertTrue(blankActivity == null || blankActivity.getName() != "");
+    }
+
+    @Test
+    public void testConstructNullTime()
+    {
+        assertTrue(nullActivity == null || nullActivity.getTime() != null);
+    }
+
+    @Test
+    public void testConstructBlankTime()
+    {
+        assertTrue(blankActivity == null || blankActivity.getTime() != "");
+    }
+
+    @Test
+    public void testConstructGoodTime()
+    {
+        assertTrue(testActivity == null || testActivity.getTime().matches("((1[0-2])|[1-9]):[0-5][0-9]\s((AM)|(PM))"));
+    }
+
+    @Test
+    public void testConstructBadTime()
+    {
+        assertTrue(badActivity == null || badActivity.getTime().matches("((1[0-2])|[1-9]):[0-5][0-9]\s((AM)|(PM))"));
     }
 }
